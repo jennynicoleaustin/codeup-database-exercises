13c52f5d3cd4b22d5b8de0413f5bf89cbbf10a5b
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-10-23 11:23:03 -0500

    where exercises part 2 complete

--- src/main/java/where_exercises.sql
+++ src/main/java/where_exercises.sql
@@ -15,3 +15,32 @@ WHERE last_name REGEXP '^E.*$';
 SELECT DISTINCT last_name
 FROM employees
 WHERE last_name LIKE '%q%';
+
+# Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN — 709 rows.
+SELECT DISTINCT first_name
+FROM employees
+WHERE first_name = 'Irena' || 'Vidya' || 'Maya';
+
+# Add a condition to the previous query to find everybody with those names who is also male — 441 rows.
+SELECT DISTINCT first_name, gender
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya') AND gender = 'M';
+
+# Find all employees whose last name starts or ends with 'E' — 30,723 rows.
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name REGEXP '^E.*$'
+OR last_name REGEXP 'e$';
+
+
+# Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name REGEXP '^E.*$'
+   AND last_name REGEXP 'e$';
+
+# Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%'
