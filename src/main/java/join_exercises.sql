2a788c23c05ed319534947ab149d33833c9a490b
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-10-24 17:44:14 -0500

    join exercises complete

--- src/main/java/join_exercises.sql
+++ src/main/java/join_exercises.sql
@@ -48,3 +48,12 @@ FROM employees as e
 WHERE dm.to_date = '9999-01-01' AND s.to_date = '9999-01-01';
 
 # Bonus Find the names of all current employees, their department name, and their current manager's name .
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS mang_name
+FROM employees as e
+         JOIN dept_emp as de
+              ON de.emp_no = e.emp_no
+         JOIN departments as d
+              ON d.dept_no = de.dept_no
+         JOIN departments as dm
+              ON d.dept_no = dm.dept_no
+WHERE de.to_date = '9999-01-01';
