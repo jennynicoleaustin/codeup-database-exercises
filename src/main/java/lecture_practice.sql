e2ec1344844300330f9f06f890aed71dc35d168e
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-10-24 15:55:38 -0500

    join lecture work through

--- src/main/java/lecture_practice.sql
+++ src/main/java/lecture_practice.sql
@@ -1,11 +1,59 @@
-USE codeup_test_db;
+# CREATE DATABASE join_test_db;
 
-CREATE TABLE authors
-(    id         INT          NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(100) NOT NULL,
-    last_name  VARCHAR(100) NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE (first_name, last_name)
-);
+USE join_test_db;
+# CREATE TABLE roles
+# (
+#     id   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+#     name VARCHAR(100) NOT NULL,
+#     PRIMARY KEY (id)
+# );
+#
+# CREATE TABLE users
+# (
+#     id      INT UNSIGNED NOT NULL AUTO_INCREMENT,
+#     name    VARCHAR(100) NOT NULL,
+#     email   VARCHAR(100) NOT NULL,
+#     role_id INT UNSIGNED DEFAULT NULL,
+#     PRIMARY KEY (id),
+#     FOREIGN KEY (role_id) REFERENCES roles (id)
+# );
+#
+# INSERT INTO roles (name)
+# VALUES ('admin');
+# INSERT INTO roles (name)
+# VALUES ('author');
+# INSERT INTO roles (name)
+# VALUES ('reviewer');
+# INSERT INTO roles (name)
+# VALUES ('commenter');
+#
+# INSERT INTO users (name, email, role_id)
+# VALUES ('bob', 'bob@example.com', 1),
+#        ('joe', 'joe@example.com', 2),
+#        ('sally', 'sally@example.com', 3),
+#        ('adam', 'adam@example.com', 3),
+#        ('jane', 'jane@example.com', null),
+#        ('mike', 'mike@example.com', null);
+
 SHOW TABLES;
-DESCRIBE authors;
+DESCRIBE roles;
+SHOW INDEX from roles;
+SELECT * from roles;
+DESCRIBE users;
+SHOW INDEX from users;
+SELECT * from users;
+
+# JOIN/ INNER JOIN
+SELECT users.name as user_name, roles.name as role_name
+FROM users
+JOIN roles ON users.role_id = roles.id;
+
+#  LEFT JOIN
+
+SELECT users.name AS user_name, roles.name AS role_name
+FROM users
+         LEFT JOIN roles ON users.role_id = roles.id;
+
+SELECT users.name AS user_name, roles.name AS role_name
+FROM users
+         RIGHT JOIN roles ON users.role_id = roles.id;
