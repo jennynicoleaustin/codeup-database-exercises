13fbe44473dc40210231a386d8c5718939950598
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-10-23 22:14:57 -0500

    aliases exercises done

--- src/main/java/aliases_exercises.sql
+++ src/main/java/aliases_exercises.sql
@@ -4,11 +4,15 @@ SELECT CONCAT(first_name, ' ', last_name) AS full_name
 FROM employees
 LIMIT 10;
 # Add the date of birth for each employee as 'DOB' to the query.
-SELECT CONCAT(first_name, ' ', last_name) AS full_name
+SELECT CONCAT(first_name, ' ', last_name, ' ', birth_date) AS full_name, 'DOB'
 FROM employees
 LIMIT 10;
 
 # Update the query to format full name to include the employee number so it is formatted as 'employee number - last name, first name'.
+SELECT CONCAT(emp_no, ' - ', last_name, ', ', first_name) AS full_name
+FROM employees
+LIMIT 10;
+
 
 # The final result should look like this:
 
