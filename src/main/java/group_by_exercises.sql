d249ed043d04e11318828db0bcaa5a7b9877c710
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-10-23 20:45:04 -0500

    group by exercise 2 done

--- src/main/java/group_by_exercises.sql
+++ src/main/java/group_by_exercises.sql
@@ -4,6 +4,11 @@ SELECT DISTINCT title
     FROM titles;
 
 # Find your query for employees whose last names start and end with 'E'. Update the query to find just the unique last names that start and end with 'E' using GROUP BY. The results should be:
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name REGEXP '^E.*$'
+   AND last_name REGEXP 'e$'
+GROUP BY last_name;
 
 # Update your previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'. You should get 846 rows.
 
