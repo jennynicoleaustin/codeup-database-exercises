c71463d7ffba418832c1ad00da311a018d8ebb86
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-10-23 20:24:20 -0500

    functions exercise 3 DONE

--- src/main/java/functions_exercises.sql
+++ src/main/java/functions_exercises.sql
@@ -18,6 +18,11 @@ WHERE month(birth_date) = 12
     OR hire_date BETWEEN 1990 AND 1999;
 
 # Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last. It should be Khun Bernini.
-
+SELECT *
+FROM employees
+WHERE month(birth_date) = 12
+    AND day(birth_date) = 25
+   OR hire_date BETWEEN 1990 AND 1999
+ORDER BY year(birth_date) ASC, hire_date ASC;
 
 # For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have been working at the company (Hint: You might also need to use now() or curdate()).
