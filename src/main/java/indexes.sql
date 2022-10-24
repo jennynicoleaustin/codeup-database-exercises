380afb9b2f4bd70392753f29df19529bfcb6308b
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-10-24 15:35:58 -0500

    proper syntax for showing the indexes included in a table

--- src/main/java/indexes.sql
+++ src/main/java/indexes.sql
@@ -3,23 +3,33 @@ USE employees;
 
 # DESCRIBE each table and inspect the keys and see which columns have indexes and keys.
 SHOW TABLES;
-DESCRIBE current_dept_emp;
-DESCRIBE departments;
-DESCRIBE dept_emp;
-DESCRIBE dept_emp_latest_date;
-DESCRIBE dept_manager;
-DESCRIBE employees;
-DESCRIBE salaries;
-DESCRIBE titles;
+SHOW INDEX from current_dept_emp;
+SHOW INDEX from departments;
+SHOW INDEX from dept_emp;
+SHOW INDEX from dept_emp_latest_date;
+SHOW INDEX from dept_manager;
+SHOW INDEX from employees;
+SHOW INDEX from salaries;
+SHOW INDEX from titles;
 # USE your codeup_test_db database.
 USE codeup_test_db;
 # Add an index to make sure all album names combined with the artist are unique. Try to add duplicates to test the constraint.
 SHOW TABLES;
 DESCRIBE albums;
 ALTER TABLE albums
-ADD UNIQUE combined_index (name, artist);
+ADD UNIQUE combine_index (name, artist);
+
 # # Verify you cannot add a duplicate for name and artist combo
+
+# This gives an error "cannot add duplicate entry"
+
 # INSERT INTO albums (artist, name, release_date, sales, genre)
 #        VALUES ('Michael Jackson', 'Thriller', 1882, 41.3, 'Pop, R&B');
+
+# While this is able to add, no error
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Michaelllllllllllllll Jackson', 'Thriller', 1882, 41.3, 'Pop, R&B');
 DESCRIBE  albums;
-# The MUL will only show on one of the columns, not both/all.
+SELECT artist, name FROM albums;
+# SHOW INDEX will show the columns with indexes
+SHOW INDEX from albums;
