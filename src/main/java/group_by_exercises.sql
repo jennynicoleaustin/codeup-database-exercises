0887c94519fbd9ca0c323d06d0c9f9c5e854e31a
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-10-23 20:51:36 -0500

    group by exercise 3 done

--- src/main/java/group_by_exercises.sql
+++ src/main/java/group_by_exercises.sql
@@ -4,17 +4,29 @@ SELECT DISTINCT title
     FROM titles;
 
 # Find your query for employees whose last names start and end with 'E'. Update the query to find just the unique last names that start and end with 'E' using GROUP BY. The results should be:
-SELECT DISTINCT last_name
+SELECT last_name
 FROM employees
 WHERE last_name REGEXP '^E.*$'
    AND last_name REGEXP 'e$'
 GROUP BY last_name;
 
 # Update your previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'. You should get 846 rows.
-
+# ????? How would I get 846 rows when the above returned 5?
 
 # Find the unique last names with a 'q' but not 'qu'. Your results should be:
+SELECT  last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
 
 # Add a COUNT() to your results and use ORDER BY to make it easier to find employees whose unusual name is shared with others.
+SELECT COUNT(last_name)
+FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%'
+GROUP BY last_name
+ORDER BY COUNT(*) DESC;
+
 
 # Update your query for 'Irena', 'Vidya', or 'Maya'. Use count(*) and GROUP BY to find the number of employees for each gender with those names. Your results should be:
